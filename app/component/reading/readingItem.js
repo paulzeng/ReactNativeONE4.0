92566d4ce72f4d72f05496f6a578263f33519480
paulzeng <372781118@qq.com>
2017-01-12 00:33:11 +0800

    1.增加阅读详情页面，带完善详情中图片的显示

--- app/component/reading/readingItem.js
+++ app/component/reading/readingItem.js
@@ -1,24 +1,31 @@
 import React,{Component} from 'react';
-import {View,StyleSheet,Image,Text,Dimensions} from 'react-native';
+import {Actions} from 'react-native-router-flux';
+import {
+    View,
+    StyleSheet,
+    Image,
+    Text,
+    Dimensions,
+    TouchableOpacity} from 'react-native';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 import commonStyle from '../../style/commonStyle';
 const tabHeight = commonStyle.TITLE_HEIHT;
 const styles = StyleSheet.create({
     container: {
-        flex:1,
+        flex: 1,
         width: windowWidth,
         paddingHorizontal: 20,
         paddingVertical: 10,
     },
     category: {
-        textAlign : 'center',
+        textAlign: 'center',
         fontSize: commonStyle.SECONDARY_FONT_SIZE,
-        paddingTop:5,
+        paddingTop: 5,
         color: commonStyle.TEXT_GRAY_COLOR
     },
     title: {
-        fontSize:30,
+        fontSize: 30,
         color: commonStyle.TEXT_COLOR,
         marginTop: 10
     },
@@ -29,20 +36,20 @@ const styles = StyleSheet.create({
         marginVertical: 20
     },
     author: {
-        fontSize:18,
+        fontSize: 18,
         color: commonStyle.TEXT_COLOR
     },
     content: {
-        fontSize:15,
+        fontSize: 15,
         color: commonStyle.TEXT_GRAY_COLOR,
-        marginVertical:20
+        marginVertical: 20
     },
     pic: {
         height: 200,
         resizeMode: 'cover',
-        justifyContent:'center',
-        alignItems:'center',
-        marginTop:20
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20
     },
     shareImage: {
         height: 20,
@@ -52,47 +59,53 @@ const styles = StyleSheet.create({
     priceImage: {
         height: 20,
         width: 20,
-        marginRight:20
+        marginRight: 20
     },
     bottomView: {
         flex: 1,
         flexDirection: 'row',
         paddingTop: 10,
         alignItems: 'flex-end',
-        justifyContent:'flex-end'
+        justifyContent: 'flex-end'
     }
 });
 
-class ReadingItem extends Component{
-    constructor(props){
+class ReadingItem extends Component {
+    constructor(props) {
         super(props);
     }
 
-    render(){
+    render() {
         let { data } = this.props;
-        return(
-            <View style={styles.container}>
-                <Text style={styles.category}>-阅读-</Text>
-                <Text style={styles.title}>{data.title}</Text>
-                <View style={styles.driver}/>
-                <Text style={styles.author}>文/{data.author ? data.author.user_name : "" }</Text>
-                <Text style={styles.content}>{data.forward}</Text>
-                <Image
-                    source = {{uri: data.img_url}}
-                    style={styles.pic}
-                />
-                <View style={styles.bottomView}>
+        return (
+            <TouchableOpacity activeOpacity={0.8}  onPress={() => this.onPress(data)}>
+                <View style={styles.container}>
+                    <Text style={styles.category}>-阅读-</Text>
+                    <Text style={styles.title}>{data.title}</Text>
+                    <View style={styles.driver}/>
+                    <Text style={styles.author}>文/{data.author ? data.author.user_name : "" }</Text>
+                    <Text style={styles.content}>{data.forward}</Text>
                     <Image
-                        style={styles.priceImage}
-                        source={require('../../image/bubble_like.png')}
+                        source={{uri: data.img_url}}
+                        style={styles.pic}
                     />
-                    <Image
-                        style={styles.shareImage}
-                        source={require('../../image/bubble_share.png')}
-                    />
+                    <View style={styles.bottomView}>
+                        <Image
+                            style={styles.priceImage}
+                            source={require('../../image/bubble_like.png')}
+                        />
+                        <Image
+                            style={styles.shareImage}
+                            source={require('../../image/bubble_share.png')}
+                        />
+                    </View>
                 </View>
-            </View>
+            </TouchableOpacity>
         );
+    }
+
+    onPress(data){
+        Actions.readingDetail({data:{data}});
     }
 }
 export default ReadingItem;
---
+++ app/data/readingDetail.js
@@ -0,0 +1,361 @@
+/**
+ * Created by Administrator on 2017/1/11.
+ * 阅读详情
+ */
+export default {
+    "res": 0,
+    "data": {
+        "content_id": "1729",
+        "hp_title": "明代江湖旅行指南：如何对付骗子、小偷、黑车司机",
+        "sub_title": "",
+        "hp_author": "",
+        "auth_it": "",
+        "hp_author_introduce": "责任编辑：小影",
+        "hp_content": "<br>\r\n<strong>在明朝浪迹天涯，又方便又便宜</strong><br>\r\n<br>\r\n从1629年开始，崇祯皇帝批准关闭了全国三分之一的驿站，以便节省每年十万两的国库开支、缓解地方民众的沉重负担。<br>\r\n<br>\r\n所有的财政收入都被挪作军用，巨大的军费开支使明朝的财政陷于恶性循环之中。<br>\r\n<br>\r\n如果有什么东西能填补驿传服务制度衰落所留下的真空的话，那只能是商业性旅游服务业的成长。它为明朝后期的私人旅行提供食宿，而花费比官营驿站的费用要低得多。<br>\r\n<br>\r\n根据1570年徽州商人黄汴出版的一本导游手册《一统路程图记》的记载，你可以在扬州城外随时跳上一艘游船，沿大运河南下，到达长江北岸的运河港口瓜州北门，船费仅需三个铜板。<br>\r\n<br>\r\n<img alt=\"\" src=\"http://image.wufazhuce.com/FsFZP7fNWBf3ZOPcFSEsfXkSwhxy?imageView2/2/w/394\" style=\"height:301px; width:394px\" /><br>\r\n▲明代南京地图<br>\r\n<br>\r\n你步行穿过瓜州城，来到该城南门，花两个铜板乘另一艘游船渡扬子江、过金山寺，来到江对岸的镇江码头。从码头入镇江城西门，向南步行1.5公里抵该城南门，再换乘另一艘游船沿运河江南段南下。<br>\r\n<br>\r\n如果你是携带行李旅行的话，码头上随时可雇到脚夫，每次只需十五个铜板。镇江以南，船费每20里（11.7公里）仅两个铜板。从镇江到太湖沿岸苏州府的吴江县，日行要换六次船。<br>\r\n<br>\r\n<img alt=\"\" src=\"http://image.wufazhuce.com/Fs0suDSPTFWZA80OC2FAy6sKgEOL?imageView2/2/w/394\" style=\"height:286px; width:394px\" /><br>\r\n▲[明] 张路《溪山放艇图》（局部）<br>\r\n<br>\r\n在吴江，你可乘日船或夜船南下到杭州。从杭州东行到宁波，香客们可乘船去普陀岛进香，旅途非常方便；云集的商船日夜往返于杭州、宁波之间。<br>\r\n<br>\r\n你要是从苏州向西到徽州府去的话，可先乘夜船到湖州，再乘另一艘夜船从湖州到位于浙江、南直隶两省交界处的四安，从那里开始转乘陆路。在四安，商行为那些去歙县的游人提供了轿子、马车、马匹等不同的交通工具。<br>\r\n<br>\r\n在商业交通繁忙的路段，商业性的旅游服务既方便又便宜。每个渡口都有成队的游船供人使用，有大批的经营旅游和货栈的商行牙人和有组织的运夫。<br>\r\n<br>\r\n<br>\r\n<strong>明代旅行发烧友如何应付扒手和黑车司机？</strong><br>\r\n<br>\r\n见月读体（1602—1679）是明朝后期东去求经继而对那个地区的佛教复兴作出贡献的众多云南僧人之一。当他1637年途经大运河镇江—苏州段的丹阳渡口时，感觉到了商行牙人间的激烈竞争。<br>\r\n<br>\r\n他被许多招徕游客的商行牙人包围起来，每个都力图游说他上自己的驳船。见月读体被这些游船游说者的嘈杂声分散了注意力，背包也被人偷走了。他向渡口官员报告了这一案件，但最终还是未能找到他的背包。<br>\r\n<br>\r\n在远离这些繁忙旅游路线的地方，旅游服务不像上面所说的那样有组织，但在那里旅行也并不难办。<br>\r\n<br>\r\n在那本著名的《徐霞客游记》中，徐弘祖（1586—1641）详细记载了他1636至1640年间从他的家乡——扬子江口附近的江阴出发到云南鸡足山佛教圣地的旅程。<br>\r\n<br>\r\n<img alt=\"\" src=\"http://image.wufazhuce.com/Fi0kbg0s2j8D7b4r1ExJze2NyP4G?imageView2/2/w/394\" style=\"height:340px; width:250px\" /><br>\r\n<br>\r\n徐氏在书中多次提到在途中雇佣船夫、车夫和运夫。1636年11月2日早晨，刚出杭州城30公里，他就发现自己雇佣的长途脚夫王二不见了。吃完早饭，他四处打听，想另雇一个人代替王二。尽管后来又雇到了一个脚夫，他们在晌午前就出发了，但徐氏还是抱怨，说这耽误了他的时间。<br>\r\n<br>\r\n在中国游人稀少的地区，想要雇到脚夫是很困难的。<br>\r\n<br>\r\n1638年6月15日，徐氏正要从贵州最西部的普安县进入云南境内，发现自己雇不到脚夫。耽搁了四天之后，来自湖广的一个商人试图为他寻募一个，但没有成功。最后，徐氏迫不得已，求助于当地的一个工头，那个工头“代为掯价”（在他6月19日的日记中，这桩买卖终于了结了，徐氏声称这个工头是他见过的三个最歹毒的人之一）。<br>\r\n<br>\r\n在他募到脚夫的那天下午，徐又雇到了一匹马，虽然价钱同样昂贵。在日记里，他愤怒地写道：“无可奈何也。”<br>\r\n<br>\r\n并不是所有偏远地区的人都热衷于为日益增长的游客提供食宿。<br>\r\n<br>\r\n据一位商人记载，在湖广，那些不愿意接待游客的村民都挂起一张牌子，上面写着一个字“去”，意思是走开。就像他向那些缺乏经验的人解说的那样：“有‘去’字之处，不可宿。”<br>\r\n<br>\r\n商业化的过程大大地改进了交通发达地区的旅游服务业，同时也加大了业已存在的这些地区与其他地区之间的社会差距——正像数量日益增长的旅游者和当地居民之间的距离一样。当地居民抵触的是他们眼中那些临时外来者的特权，并以其他的反抗形式表现出来。<br>\r\n<br>\r\n在福建沿海山区，一般认为乘牲畜或轿子的游人在到达一个村落时应该下马或下轿，步行穿过村庄，然后才能上马（轿）。那些拒绝遵循当地习俗的人，要么在挖苦和嘲笑声中滚鞍落马（轿），要么就会被村民们用石头袭击。<br>\r\n<br>\r\n这种习俗好像是在明代后期出现，当时越来越多的人开始流动。如果这还算不上是抵触的话，那至少可以说是旅游者可以感受到的当地居民对他们的冷漠和不尊重。<br>\r\n<br>\r\n这样，无力旅游的人的冷漠态度或多或少地挫掉那些有钱旅游者的一些锐气。至少在周边地区，当地人拒绝与外地人融合，并且一般来说都拒绝向富有的旅行者献殷勤。<br>\r\n<br>\r\n<br>\r\n<strong>在明朝的北方旅行，要请保镖防土匪</strong><br>\r\n<br>\r\n对陆上旅行来说，更大的阻碍是盗匪的活动，对盗匪的担忧在明代后期的著作中屡有记载。人们深深地认识到，在许多地区他们不敢在夜间旅行，甚至在游人很多的江南旅游中心地带也是如此。<br>\r\n<br>\r\n根据《一统路程图记》作者黄汴1570年的记载，太湖南部地区非常安全，大多数船只都是夜间从杭州起航，将商人连同他们日间做买卖的所得安全送抵目的地。但是苏州以北就没有夜船航行，在东边的松江府也没有任何夜间摆渡服务。<br>\r\n<br>\r\n<img alt=\"\" src=\"http://image.wufazhuce.com/FtlTKE8USoWKhKdbryt5PEypuD09?imageView2/2/w/394\" style=\"height:293px; width:394px\" /><br>\r\n▲明代全国交通地图<br>\r\n<br>\r\n半个世纪以后，1626年的另一本导游手册的作者对这一地区就不那么有信心了。<br>\r\n<br>\r\n那时的政局似乎不太稳定。他也赞同在湖州正南面靠近杭州的人口密集区旅行是很安全的，但同时也告诫人们不要在黎明或黄昏时旅行，更不能在荒歉时期到那里去。至于苏州周围地区，他写道，在歉收时期这里盗匪猖獗，只有在保镖的陪同下才能在苏州的农村地区旅行。<br>\r\n<br>\r\n在长江以北地区旅行更不安全。黄汴说，扬州以南大运河和扬子江交汇处是著名的盐、棉商人聚集地，那里无论昼夜都没有匪徒；但他警告说，扬州以北地区的食盐走私活动使人们不可能夜间在大运河上旅行。他提醒商人们雇佣当地船夫时要小心，因为他们都很不诚实。<br>\r\n<br>\r\n<img alt=\"\" src=\"http://image.wufazhuce.com/FoTUo8hL0648rjgFhssW6kj-qsq1?imageView2/2/w/394\" style=\"height:486px; width:352px\" /><br>\r\n▲一艘盖有草席遮蓬的小渡船。这幅图画的是《水浒传》中一百零八将之一的张顺，在对上次渡河时偷过他东西的船夫进行报复。<br>\r\n<br>\r\n在黄河以北，问题不是你雇佣的人会偷你的货物，而是他们往往先和你签约领取高额的佣金用来还债，然后在去京城的半路上人就没影了。在从天津到京城间的最后路段上，旅途再度变得安全起来。你可以夜间在这段路上旅行，尽管黄汴还是劝你多加小心。<br>\r\n<br>\r\n在上述路程的北段，黄汴说光小心还不够，他建议招募一位携带武器的保镖，来防卫游荡于华北平原上的马匪。甚至在一些地方（他说到从颖州到大名的路段），即使是武装保镖也不见得够用。<br>\r\n<br>\r\n与之相较，从大运河经开封到山西南部的商人就不会遇到这样的问题。<br>\r\n<br>\r\n位于清化镇——对于南下贸易的山西商人来说，它是河南北部的主要交通枢纽——以西的地区，旅程非常安全。<br>\r\n<br>\r\n对此，黄汴肯定地说，你不但可以夜间旅行，而且可以在月色下从容赶路，因为来往不绝的商业运输使那里的劫掠行为要冒很大的风险。<br>\r\n<br>\r\n内容整理自卜正民的经典之作、美国汉学最高奖项列文森奖获奖作品《纵乐的困惑: 明代的商业与文化》",
+        "hp_makettime": "2010-01-01 02:00:00",
+        "hide_flag": "0",
+        "wb_name": "",
+        "wb_img_url": "",
+        "last_update_date": "2017-01-10 18:26:14",
+        "web_url": "http://m.wufazhuce.com/article/1729",
+        "guide_word": "在很久很久以前的明代，如果要出门旅行，人们会碰到什么问题？",
+        "audio": "",
+        "anchor": "",
+        "editor_email": "zhuxiaoying@wufazhuce.com",
+        "top_media_type": "0",
+        "top_media_file": "",
+        "top_media_image": "",
+        "start_video": "",
+        "copyright": "本文来源“视知”，付费转载自理想国imaginist",
+        "author": [
+            {
+                "user_id": "0",
+                "user_name": "卜正民",
+                "web_url": "",
+                "summary": "",
+                "desc": "",
+                "is_settled": "",
+                "settled_type": "",
+                "fans_total": "",
+                "wb_name": ""
+            }
+        ],
+        "maketime": "2010-01-01 02:00:00",
+        "author_list": [
+            {
+                "user_id": "0",
+                "user_name": "卜正民",
+                "web_url": "",
+                "summary": "",
+                "desc": "",
+                "is_settled": "",
+                "settled_type": "",
+                "fans_total": "",
+                "wb_name": ""
+            }
+        ],
+        "next_id": "1683",
+        "previous_id": "1691",
+        "praisenum": 91,
+        "sharenum": 9,
+        "commentnum": 14,
+        "conment_data": [
+            {
+                "id": "33369",
+                "quote": "",
+                "content": "很有趣啊",
+                "praisenum": 0,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "0",
+                "user_info_id": "0",
+                "input_date": "2017-01-11 22:26:30",
+                "created_at": "2017-01-11 22:26:30",
+                "updated_at": "0000-00-00 00:00:00",
+                "user": {
+                    "user_id": "5157902",
+                    "user_name": "像河一样",
+                    "web_url": "http://tp2.sinaimg.cn/5640337877/180/5730270536/1"
+                },
+                "touser": null,
+                "type": 1
+            },
+            {
+                "id": "33367",
+                "quote": "",
+                "content": "想去走一遍",
+                "praisenum": 1,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "0",
+                "user_info_id": "0",
+                "input_date": "2017-01-11 21:09:22",
+                "created_at": "2017-01-11 21:09:22",
+                "updated_at": "0000-00-00 00:00:00",
+                "user": {
+                    "user_id": "6309739",
+                    "user_name": "清醒",
+                    "web_url": "http://image.wufazhuce.com/FrVioXgYKNZOMQZJd2W0TEQvkTT8"
+                },
+                "touser": null,
+                "type": 1
+            },
+            {
+                "id": "33363",
+                "quote": "请尊重别人，每个人可以发表自己的意见。管好你的嘴巴",
+                "content": "他也是在发表他的意见吧，而且很客观",
+                "praisenum": 0,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "1",
+                "user_info_id": "10097",
+                "input_date": "2017-01-11 18:46:35",
+                "created_at": "2017-01-11 18:46:35",
+                "updated_at": "2017-01-11 20:12:46",
+                "user": {
+                    "user_id": "6049893",
+                    "user_name": "kiven",
+                    "web_url": "http://q.qlogo.cn/qqapp/1104596227/98B404D2C6E1EC993F7910E0C239AE23/100"
+                },
+                "touser": {
+                    "user_id": "7648550",
+                    "user_name": "吴光鹏",
+                    "web_url": "http://q.qlogo.cn/qqapp/1104596227/0E0744BBCB32082C4DADF2B6486520FE/100"
+                },
+                "type": 1
+            },
+            {
+                "id": "33361",
+                "quote": "",
+                "content": "古时候旅游都要冒这么多风险。",
+                "praisenum": 3,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "1",
+                "user_info_id": "10097",
+                "input_date": "2017-01-11 16:16:33",
+                "created_at": "2017-01-11 16:16:33",
+                "updated_at": "2017-01-11 20:16:46",
+                "user": {
+                    "user_id": "1060041",
+                    "user_name": "吾御成疯归去",
+                    "web_url": "http://image.wufazhuce.com/FpaXH2elWfdiUrfXYrFrwDVbQ_nE"
+                },
+                "touser": null,
+                "type": 1
+            },
+            {
+                "id": "33360",
+                "quote": "我倒觉得这样的改版挺好的呀，在改版接受新事物前能静下心来比较深入地去了解，而不是先喷，应该说去年（最最早的三四年前还没有阅读只有一图一话）我不会戳开one的阅读，因为都是那种看题目和摘要就腻了的一个腔调的文章，现在这样反而能做些比较深入也有趣的阅读",
+                "content": "请尊重别人，每个人可以发表自己的意见。管好你的嘴巴",
+                "praisenum": 0,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "1",
+                "user_info_id": "10097",
+                "input_date": "2017-01-11 15:09:11",
+                "created_at": "2017-01-11 15:09:11",
+                "updated_at": "2017-01-11 20:18:28",
+                "user": {
+                    "user_id": "7648550",
+                    "user_name": "吴光鹏",
+                    "web_url": "http://q.qlogo.cn/qqapp/1104596227/0E0744BBCB32082C4DADF2B6486520FE/100"
+                },
+                "touser": {
+                    "user_id": "7040993",
+                    "user_name": "谢小P吃一朵花_",
+                    "web_url": "http://tva4.sinaimg.cn/crop.0.0.996.996.180/95d99f16jw8f3ql6atb1jj20ro0rpabn.jpg"
+                },
+                "type": 1
+            },
+            {
+                "id": "33359",
+                "quote": "",
+                "content": "🔆",
+                "praisenum": 0,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "1",
+                "user_info_id": "10097",
+                "input_date": "2017-01-11 13:35:25",
+                "created_at": "2017-01-11 13:35:25",
+                "updated_at": "2017-01-11 20:22:36",
+                "user": {
+                    "user_id": "5388360",
+                    "user_name": "执拗.",
+                    "web_url": "http://image.wufazhuce.com/FtYHLY63LrPsmHC1wwuNiBxPov83"
+                },
+                "touser": null,
+                "type": 1
+            },
+            {
+                "id": "33357",
+                "quote": "",
+                "content": "认真看完的人也够无聊的。。。。",
+                "praisenum": 6,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "1",
+                "user_info_id": "10097",
+                "input_date": "2017-01-11 12:43:47",
+                "created_at": "2017-01-11 12:43:47",
+                "updated_at": "2017-01-11 20:24:47",
+                "user": {
+                    "user_id": "5482004",
+                    "user_name": "乞人",
+                    "web_url": "http://image.wufazhuce.com/FiFhvrdP_R-mi-7Kwo7xKv9s7vzn"
+                },
+                "touser": null,
+                "type": 1
+            },
+            {
+                "id": "33354",
+                "quote": "",
+                "content": "是 都要亡国了 旅行去",
+                "praisenum": 0,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "1",
+                "user_info_id": "10097",
+                "input_date": "2017-01-11 11:01:50",
+                "created_at": "2017-01-11 11:01:50",
+                "updated_at": "2017-01-11 20:32:28",
+                "user": {
+                    "user_id": "864669",
+                    "user_name": "芬梨道上",
+                    "web_url": "http://image.wufazhuce.com/Fg2teFD9a985Wnm9yZnRnCuIzB9s"
+                },
+                "touser": null,
+                "type": 1
+            },
+            {
+                "id": "33352",
+                "quote": "这么多天依旧不习惯如今的版本，感觉没了曾经那种感觉，如今的one 一个，很陌生很陌生，有一种卸载了的冲动，，，",
+                "content": "想卸载就卸载啊，你说一万遍，一个还是这样的一个，依旧向前",
+                "praisenum": 1,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "1",
+                "user_info_id": "10097",
+                "input_date": "2017-01-11 10:47:11",
+                "created_at": "2017-01-11 10:47:11",
+                "updated_at": "2017-01-11 20:33:14",
+                "user": {
+                    "user_id": "7582744",
+                    "user_name": "*** **** 7939",
+                    "web_url": "http://image.wufazhuce.com/app3.0head.png"
+                },
+                "touser": {
+                    "user_id": "4854184",
+                    "user_name": "喜欢一个她",
+                    "web_url": "http://qzapp.qlogo.cn/qzapp/1104596227/275BA074225D8343F21A6909D1F3FCC7/100"
+                },
+                "type": 1
+            },
+            {
+                "id": "33351",
+                "quote": "",
+                "content": "我倒觉得这样的改版挺好的呀，在改版接受新事物前能静下心来比较深入地去了解，而不是先喷，应该说去年（最最早的三四年前还没有阅读只有一图一话）我不会戳开one的阅读，因为都是那种看题目和摘要就腻了的一个腔调的文章，现在这样反而能做些比较深入也有趣的阅读",
+                "praisenum": 8,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "1",
+                "user_info_id": "10093",
+                "input_date": "2017-01-11 10:18:26",
+                "created_at": "2017-01-11 10:18:26",
+                "updated_at": "2017-01-11 10:22:08",
+                "user": {
+                    "user_id": "7040993",
+                    "user_name": "谢小P吃一朵花_",
+                    "web_url": "http://tva4.sinaimg.cn/crop.0.0.996.996.180/95d99f16jw8f3ql6atb1jj20ro0rpabn.jpg"
+                },
+                "touser": null,
+                "type": 1
+            },
+            {
+                "id": "33350",
+                "quote": "",
+                "content": "一点也不有趣",
+                "praisenum": 1,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "1",
+                "user_info_id": "10093",
+                "input_date": "2017-01-11 09:58:30",
+                "created_at": "2017-01-11 09:58:30",
+                "updated_at": "2017-01-11 10:04:20",
+                "user": {
+                    "user_id": "7666433",
+                    "user_name": "朱博宇",
+                    "web_url": "http://wx.qlogo.cn/mmopen/Xmnun9Io49TaHVibKGh8HfBbvZeFOEoTWMs4pnEYnicgkwPtd5tyUwr1TndNHu8cF0SPDvJh2FePuu94S1cGWjzw/0"
+                },
+                "touser": null,
+                "type": 1
+            },
+            {
+                "id": "33349",
+                "quote": "",
+                "content": "喂，110吗？",
+                "praisenum": 2,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "1",
+                "user_info_id": "10093",
+                "input_date": "2017-01-11 07:30:40",
+                "created_at": "2017-01-11 07:30:40",
+                "updated_at": "2017-01-11 10:05:28",
+                "user": {
+                    "user_id": "6703170",
+                    "user_name": "許、諾",
+                    "web_url": "http://image.wufazhuce.com/Fhz5BMZPcYi6am2083uknuB4rXbB"
+                },
+                "touser": null,
+                "type": 1
+            },
+            {
+                "id": "33348",
+                "quote": "这么多天依旧不习惯如今的版本，感觉没了曾经那种感觉，如今的one 一个，很陌生很陌生，有一种卸载了的冲动，，，",
+                "content": "隔了好几天不看，今天来看会不会有新发现，可惜了……我就先走了，希望明年回心转意再装上的时候能看到你的回复。",
+                "praisenum": 1,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "1",
+                "user_info_id": "10093",
+                "input_date": "2017-01-11 06:57:52",
+                "created_at": "2017-01-11 06:57:52",
+                "updated_at": "2017-01-11 10:12:27",
+                "user": {
+                    "user_id": "1398850",
+                    "user_name": "怪-化猫",
+                    "web_url": "http://tp2.sinaimg.cn/2518018705/180/5661395657/1"
+                },
+                "touser": {
+                    "user_id": "4854184",
+                    "user_name": "喜欢一个她",
+                    "web_url": "http://qzapp.qlogo.cn/qzapp/1104596227/275BA074225D8343F21A6909D1F3FCC7/100"
+                },
+                "type": 1
+            },
+            {
+                "id": "33347",
+                "quote": "",
+                "content": "这么多天依旧不习惯如今的版本，感觉没了曾经那种感觉，如今的one 一个，很陌生很陌生，有一种卸载了的冲动，，，",
+                "praisenum": 2,
+                "device_token": "",
+                "del_flag": "0",
+                "reviewed": "1",
+                "user_info_id": "10093",
+                "input_date": "2017-01-11 05:23:24",
+                "created_at": "2017-01-11 05:23:24",
+                "updated_at": "2017-01-11 10:01:27",
+                "user": {
+                    "user_id": "4854184",
+                    "user_name": "喜欢一个她",
+                    "web_url": "http://qzapp.qlogo.cn/qzapp/1104596227/275BA074225D8343F21A6909D1F3FCC7/100"
+                },
+                "touser": null,
+                "type": 1
+            }
+        ]
+    }
+}
